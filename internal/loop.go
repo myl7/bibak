0a827f37bf8e47f84a3816400c77658202e4bf5d
myl7 <myl@myl.moe>
2022-01-24 00:39:46 +0800

    update error handling and logging

--- internal/loop.go
+++ internal/loop.go
@@ -2,21 +2,32 @@ package internal
 
 import (
 	"github.com/myl7/bibak/internal/config"
+	"log"
+	"net/http"
+	"runtime/debug"
 	"time"
 )
 
-func Loop() error {
+func Loop() {
 	for true {
-		err := round()
-		if err != nil {
-			return err
-		}
+		func() {
+			defer func() {
+				err := recover()
+				if err != nil {
+					log.Println("error:", err.(error), "stacktrace:\n", string(debug.Stack()))
+				}
+			}()
+
+			round()
+		}()
 
 		time.Sleep(config.Cfg.LoopInterval)
 	}
-	return nil
 }
 
-func round() error {
-	return nil
+func round() {
+	res, err := http.Get(config.Cfg.RsshubUrl)
+	if err != nil {
+		panic(err)
+	}
 }
